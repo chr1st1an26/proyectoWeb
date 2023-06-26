095d5c75dbd021f8a1c64caf0ddbcaa5cca8a4a2
chr1st1an26 <crashemanuel84@gmail.com>
2023-06-26 17:13:54 -0600

    26-06-23
    crwcion de algo xd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { BrowserModule } from '@angular/platform-browser';
-
+// import {AngularFireModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
@@ -13,6 +14,21 @@ import { SociosComponent } from './componentes/socios/socios.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { ProductosComponent } from './componentes/productos/productos.component';
+import { FormsModule } from '@angular/forms';
+import {AngularFireModule} from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCctzT1dCjSnbdo2oMbLc6_3GGYJYJr_GM",
+  authDomain: "fir-bd-757f6.firebaseapp.com",
+  projectId: "fir-bd-757f6",
+  storageBucket: "fir-bd-757f6.appspot.com",
+  messagingSenderId: "215459649770",
+  appId: "1:215459649770:web:fac056ecdf1920d16c5f93",
+  measurementId: "G-1SZ21MJM9Z"
+};
 
 @NgModule({
   declarations: [
@@ -30,9 +46,15 @@ import { ProductosComponent } from './componentes/productos/productos.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig)
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormsModule } from '@angular/forms';
+// import {AngularFireModule} from '@angular/fire/compat';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
 
 @Component({
   selector: 'app-login',
@@ -7,4 +13,38 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  recordsRef: AngularFireList<any>;
+  private dbPath = '/users';
+
+  constructor(private db: AngularFireDatabase) {
+    this.recordsRef = db.list<any>(this.dbPath);
+  }
+
+  usuario = {
+    email: '',
+    password: ''
+  }
+
+  
+
+
+  Ingresar() {
+    this.recordsRef.push(this.usuario);
+  }
+
+  createRecord(record: any): void {
+    this.recordsRef.push(record);
+  }
+  
+  updateRecord(key: string, value: any): Promise<void> {
+    return this.recordsRef.update(key, value);
+  }
+  
+  deleteRecord(key: string): Promise<void> {
+    return this.recordsRef.remove(key);
+  }
+  
+  getRecordsList(): AngularFireList<any> {
+    return this.recordsRef;
+  }
 }
